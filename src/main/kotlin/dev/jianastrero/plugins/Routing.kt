4cc35dcb7445ca3cd84b0c8bb0c4243d14a32a59
Jian James Astrero <jianjamesastrero@gmail.com>
2023-08-11 03:43:51 +0800

    Refactor imports and add new route for CSS
    Modified file 'Routing.kt' resolves 'import *' issue, making all imports explicit for better code readability and maintainability. Also, route '/font.css' was added for serving potential CSS assets, extending application's capability to handle css requests.

--- src/main/kotlin/dev/jianastrero/plugins/Routing.kt
+++ src/main/kotlin/dev/jianastrero/plugins/Routing.kt
@@ -1,12 +1,18 @@
 package dev.jianastrero.plugins
 
-import io.ktor.http.*
-import io.ktor.server.application.*
-import io.ktor.server.http.content.*
-import io.ktor.server.plugins.autohead.*
-import io.ktor.server.plugins.statuspages.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
+import dev.jianastrero.css.font
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.application.Application
+import io.ktor.server.application.call
+import io.ktor.server.application.install
+import io.ktor.server.http.content.resources
+import io.ktor.server.http.content.static
+import io.ktor.server.plugins.autohead.AutoHeadResponse
+import io.ktor.server.plugins.statuspages.StatusPages
+import io.ktor.server.response.respondText
+import io.ktor.server.routing.get
+import io.ktor.server.routing.routing
+import kotlinx.css.CSSBuilder
 
 fun Application.configureRouting() {
     install(AutoHeadResponse)
@@ -18,6 +24,9 @@ fun Application.configureRouting() {
     routing {
         get("/") {
             call.respondText("Hello Jian!")
+        }
+        get("/font.css") {
+            call.respondCss(CSSBuilder::font)
         }
         // Static plugin. Try to access `/static/index.html`
         static("/static") {
