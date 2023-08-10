0d2a7d80a0a541b13868cd70f147bcfd813be616
Jian James Astrero <jianjamesastrero@gmail.com>
2023-08-11 03:44:26 +0800

    Add MyTemplate.kt for HTML templating
    Introduced a new file 'MyTemplate.kt' under 'dev.jianastrero.pages.template' for handling HTML templating. This abstracts HTML structure and provides better management of head and body elements for different pages. This will facilitate future work on UI and improve overall project structure.

---
+++ src/main/kotlin/dev/jianastrero/pages/template/MyTemplate.kt
@@ -0,0 +1,37 @@
+package dev.jianastrero.pages.template
+
+import dev.jianastrero.theme.defineFont
+import io.ktor.server.html.Placeholder
+import io.ktor.server.html.Template
+import io.ktor.server.html.insert
+import kotlinx.html.BODY
+import kotlinx.html.HEAD
+import kotlinx.html.HTML
+import kotlinx.html.body
+import kotlinx.html.head
+import kotlinx.html.lang
+import kotlinx.html.meta
+import kotlinx.html.title
+
+class MyTemplate(val title: String) : Template<HTML> {
+    val head = Placeholder<HEAD>()
+    val body = Placeholder<BODY>()
+    override fun HTML.apply() {
+        lang = "en"
+
+        head {
+            meta(charset = "utf-8")
+            title { +this@MyTemplate.title }
+            meta(name = "viewport", content = "width=device-width, initial-scale=1.0")
+            meta(name = "description", content = "Jian Astrero's Portfolio")
+            meta(name = "author", content = "Jian Astrero")
+            meta(name = "keywords", content = "Jian Astrero, Jian, Astrero, Portfolio, Jian Astrero's Portfolio")
+            defineFont()
+            insert(this@MyTemplate.head)
+        }
+
+        body {
+            insert(this@MyTemplate.body)
+        }
+    }
+}
