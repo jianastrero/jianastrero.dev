81ca0da8f5e3ffa12ddf66eafaba6ec06fff4998
Jian James Astrero <jianjamesastrero@gmail.com>
2023-08-11 13:14:18 +0800

    Rename and refactor MyTemplate to BaseTemplate
    The class MyTemplate.kt was renamed to SiteTemplate.kt and refactored to extend from the new BaseTemplate class. This improves reusability by inheriting from BaseTemplate which has common HTML head and body properties. The change intends to keep the code base DRY and make it easier to manage common website template properties.

---
+++ src/main/kotlin/dev/jianastrero/pages/template/SiteTemplate.kt
@@ -0,0 +1,14 @@
+package dev.jianastrero.pages.template
+
+import dev.jianastrero.templating.BaseTemplate
+
+class SiteTemplate(title: String) : BaseTemplate(title) {
+
+//    override fun HTML.apply() {
+//        insert(this@SiteTemplate) {
+//            head {
+//                defineFont()
+//            }
+//        }
+//    }
+}
