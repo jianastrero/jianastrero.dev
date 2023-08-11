e1fd97778a8706645f4d2ecdcce09db65c0a5fee
Jian James Astrero <jianjamesastrero@gmail.com>
2023-08-11 13:24:59 +0800

    Update import reference in FontCss.kt
    Renamed CSSBuilder to CssBuilder in the import statement and invocation in order to align with other import convention. This approach makes the code readable and consistent across the entire codebase.

--- src/main/kotlin/dev/jianastrero/css/FontCss.kt
+++ src/main/kotlin/dev/jianastrero/css/FontCss.kt
@@ -4,16 +4,16 @@ import dev.jianastrero.plugins.respondCss
 import io.ktor.server.application.call
 import io.ktor.server.routing.Routing
 import io.ktor.server.routing.get
-import kotlinx.css.CSSBuilder
+import kotlinx.css.CssBuilder
 import kotlinx.css.fontFamily
 
 fun Routing.fontCss() {
     get("/font.css") {
-        call.respondCss(CSSBuilder::fontCss)
+        call.respondCss(CssBuilder::fontCss)
     }
 }
 
-private fun CSSBuilder.fontCss() {
+private fun CssBuilder.fontCss() {
     rule("*") {
         fontFamily = "Montserrat, sans-serif"
     }
