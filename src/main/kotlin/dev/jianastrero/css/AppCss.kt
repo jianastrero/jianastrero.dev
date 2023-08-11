5db05d6d124fbebd84664ac7ea887ef1a06e6aeb
Jian James Astrero <jianjamesastrero@gmail.com>
2023-08-11 13:25:09 +0800

    Enhance AppCss.kt with styling rules
    Added specific styling rules to the appCss function in AppCss.kt. CSS Builder reference was updated to CssBuilder for consistency and new rules for ".section", ".text-weight-black", ".text-xlarge" and ".text-stroke" were added to improve the design and layout of the application.

--- src/main/kotlin/dev/jianastrero/css/AppCss.kt
+++ src/main/kotlin/dev/jianastrero/css/AppCss.kt
@@ -4,14 +4,46 @@ import dev.jianastrero.plugins.respondCss
 import io.ktor.server.application.call
 import io.ktor.server.routing.Routing
 import io.ktor.server.routing.get
-import kotlinx.css.CSSBuilder
+import kotlinx.css.Border
+import kotlinx.css.BorderStyle
+import kotlinx.css.Color
+import kotlinx.css.CssBuilder
+import kotlinx.css.alignItems
+import kotlinx.css.backgroundColor
+import kotlinx.css.border
+import kotlinx.css.display
+import kotlinx.css.flexDirection
+import kotlinx.css.fontSize
+import kotlinx.css.fontWeight
+import kotlinx.css.justifyContent
+import kotlinx.css.minHeight
+import kotlinx.css.px
+import kotlinx.css.rem
+import kotlinx.css.vh
 
 fun Routing.appCss() {
     get("/app.css") {
-        call.respondCss(CSSBuilder::appCss)
+        call.respondCss(CssBuilder::appCss)
     }
 }
 
-private fun CSSBuilder.appCss() {
-
+private fun CssBuilder.appCss() {
+    rule(".section") {
+        display = kotlinx.css.Display.flex
+        flexDirection = kotlinx.css.FlexDirection.column
+        alignItems = kotlinx.css.Align.center
+        justifyContent = kotlinx.css.JustifyContent.center
+        minHeight = 100.vh
+    }
+    rule(".text-weight-black") {
+        fontWeight = kotlinx.css.FontWeight.w900
+    }
+    rule(".text-xlarge") {
+        fontSize = 8.rem
+    }
+    rule(".text-stroke") {
+        backgroundColor = Color.transparent
+        border = Border(1.px, BorderStyle.solid, Color.black)
+//        setCustomProperty("text-stroke", CssValue("1px solid black"))
+    }
 }
