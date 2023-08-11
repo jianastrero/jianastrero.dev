c24f68c2dde83330a0fd476779be2ef953b3020e
Jian James Astrero <jianjamesastrero@gmail.com>
2023-08-11 13:53:08 +0800

    Update SiteTemplate to improve structure and flexibility
    Updated the SiteTemplate file to inherit and implement functions from the BaseTemplate as abstract methods. This change defines `HEAD.beforeHead()`, `HEAD.afterHead()`, `BODY.beforeBody()`, and `BODY.afterBody()` to enable custom functionality. Furthermore, the `defineFont()` function is now being called in `HEAD.beforeHead()`. Made these changes to offer better control over the structure of pages, increasing the flexibility and usability of templates.

--- src/main/kotlin/dev/jianastrero/pages/template/SiteTemplate.kt
+++ src/main/kotlin/dev/jianastrero/pages/template/SiteTemplate.kt
@@ -1,14 +1,18 @@
 package dev.jianastrero.pages.template
 
 import dev.jianastrero.templating.BaseTemplate
+import dev.jianastrero.theme.defineFont
+import kotlinx.html.BODY
+import kotlinx.html.HEAD
 
 class SiteTemplate(title: String) : BaseTemplate(title) {
+    override fun HEAD.beforeHead() {
+        defineFont()
+    }
 
-//    override fun HTML.apply() {
-//        insert(this@SiteTemplate) {
-//            head {
-//                defineFont()
-//            }
-//        }
-//    }
+    override fun HEAD.afterHead() {}
+
+    override fun BODY.beforeBody() {}
+
+    override fun BODY.afterBody() {}
 }
