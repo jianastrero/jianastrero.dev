2f06567a83730b5a5012b03abad5fc53ccb9e76e
Jian James Astrero <jianjamesastrero@gmail.com>
2023-08-12 04:58:17 +0800

    Refactor style rules in AppCss.kt
    Refactored various style rules in AppCss.kt for enhanced UI/UX. The modifications include removing unused properties, adding margins and padding to 'body', and defining a more consistent color scheme. Additionally, introduced 'default-gradient' class for an elegant conic-gradient background and updated the color of all elements to improve readability on the NOT FOUND page.

--- src/main/kotlin/dev/jianastrero/css/AppCss.kt
+++ src/main/kotlin/dev/jianastrero/css/AppCss.kt
@@ -4,19 +4,21 @@ import dev.jianastrero.plugins.respondCss
 import io.ktor.server.application.call
 import io.ktor.server.routing.Routing
 import io.ktor.server.routing.get
-import kotlinx.css.Border
-import kotlinx.css.BorderStyle
 import kotlinx.css.Color
 import kotlinx.css.CssBuilder
+import kotlinx.css.Margin
+import kotlinx.css.Padding
 import kotlinx.css.alignItems
-import kotlinx.css.backgroundColor
-import kotlinx.css.border
+import kotlinx.css.background
+import kotlinx.css.color
 import kotlinx.css.display
 import kotlinx.css.flexDirection
 import kotlinx.css.fontSize
 import kotlinx.css.fontWeight
 import kotlinx.css.justifyContent
+import kotlinx.css.margin
 import kotlinx.css.minHeight
+import kotlinx.css.padding
 import kotlinx.css.px
 import kotlinx.css.rem
 import kotlinx.css.vh
@@ -28,6 +30,11 @@ fun Routing.appCss() {
 }
 
 private fun CssBuilder.appCss() {
+    rule("body") {
+        margin = Margin(0.px)
+        padding = Padding(0.px)
+        background = "#000000"
+    }
     rule(".section") {
         display = kotlinx.css.Display.flex
         flexDirection = kotlinx.css.FlexDirection.column
@@ -41,9 +48,10 @@ private fun CssBuilder.appCss() {
     rule(".text-xlarge") {
         fontSize = 8.rem
     }
-    rule(".text-stroke") {
-        backgroundColor = Color.transparent
-        border = Border(1.px, BorderStyle.solid, Color.black)
-//        setCustomProperty("text-stroke", CssValue("1px solid black"))
+    rule(".default-gradient") {
+        background = "conic-gradient(from 220deg at 35.12% 73.82%, #001C30 0deg, rgba(0, 28, 48, 0.00) 344.6943712234497deg)"
+    }
+    rule("*") {
+        color = Color("#D1DAE0")
     }
 }
